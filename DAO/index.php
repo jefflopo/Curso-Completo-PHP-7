ab240868d00c5ef20d1226dc98e201b72a06abaa
jefflopo <jeffersonlopo@gmail.com>
2017-03-09 12:11:43 -0300

    Acrescentado exemplo de carregamento para o id 3

--- DAO/index.php
+++ DAO/index.php
@@ -1,8 +1,14 @@
 <?php
     require_once ("config.php");
     
-    $sql = new Sql();
-    $usuarios = $sql->select("SELECT * FROM tb_usuarios");
+//    $sql = new Sql();
+//    $usuarios = $sql->select("SELECT * FROM tb_usuarios");
+//    
+//    echo json_encode($usuarios);
     
-    echo json_encode($usuarios);
+    $root = new Usuario();
+    
+    $root->loadById( 3 );
+    
+    echo $root;
 
