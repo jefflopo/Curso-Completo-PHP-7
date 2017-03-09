b5befa8fa2f1b4a844f1387e0aaed362340b8bf3
jefflopo <jeffersonlopo@gmail.com>
2017-03-09 12:38:28 -0300

    Testado carregamento dos dados de usuario atraves do login e senha

--- DAO/index.php
+++ DAO/index.php
@@ -6,9 +6,21 @@
 //    
 //    echo json_encode($usuarios);
     
-    $root = new Usuario();
+    // apenas carrega um usuário específico
+//    $root = new Usuario();    
+//    $root->loadById( 3 );    
+//    echo $root;
     
-    $root->loadById( 3 );
+    // Carrega uma lista de usuarios
+    //$lista = Usuario::getList();
+    //echo json_encode($lista);
     
-    echo $root;
+    // Carrega uma lista de usuarios buscando pelo login
+    //$search = Usuario::search("jo");
+    //echo json_encode( $search );
 
+    // Carrega um usuario usando o login e a senha
+    $usuario = new Usuario();
+    $usuario->login("root", "54321");
+    
+    echo $usuario;
