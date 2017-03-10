c2d0e048173307404ba71280595a0e9e1b8fb1d6
jefflopo <jeffersonlopo@gmail.com>
2017-03-10 12:12:20 -0300

    Criado arquivo usuarios.csv para guardar os dados extraidos do BD

---
+++ FOPEN/exemplo-02.php
@@ -0,0 +1,34 @@
+<?php
+    require_once ("config.php");
+    
+    $sql = new Sql();
+    
+    $usuarios = $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+    
+    $headers = array();
+    
+    foreach ($usuarios[0] as $key => $value) {
+        
+        array_push($headers, ucfirst($key));
+        
+    }
+    
+    $file = fopen("usuarios.csv","w+");
+    fwrite($file, implode(",", $headers) . "\r\n" );
+    
+    foreach ($usuarios as $row) {
+        
+        $data = array();
+        
+        foreach ($row as $key => $value) {
+            
+            array_push($data, $value);
+            
+        } //fim do foreach de coluna
+        
+        fwrite($file, implode(",", $data) . "\r\n");
+        
+    } // fim do foreach de linha
+    
+    fclose($file);
+
