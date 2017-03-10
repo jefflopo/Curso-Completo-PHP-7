295c5efa129ef6851cd112494dedc706aa71833f
jefflopo <jeffersonlopo@gmail.com>
2017-03-10 12:37:54 -0300

    Exemplo de exclusao - UNLINK - de arquivo

---
+++ UNLINK/exemplo-02.php
@@ -0,0 +1,12 @@
+<?php
+    if(!is_dir("images")) {
+        mkdir("images");
+    }
+    
+    foreach (scandir("images") as $item) {
+        if(!in_array($item, array(".", ".."))){
+            unlink("images/" .$item);
+        }
+    }
+        
+
