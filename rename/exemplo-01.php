a0d7079df68ea664ca0538f18cd2e8eee84fb800
jefflopo <jeffersonlopo@gmail.com>
2017-03-13 13:44:02 -0300

    Criada a pasta rename com um arquivo de exemplo para mover arquivo de diretorios distintos

---
+++ rename/exemplo-01.php
@@ -0,0 +1,30 @@
+<?php
+    $dir1 = "folder_01";
+    $dir2 - "folder_02";
+    
+    if( !is_dir($dir1) ) {
+        mkdir ($dir1);
+    }
+    if( !is_dir($dir2) ) {
+        mkdir ($dir2);
+    }
+    
+    $filename = "README.txt";
+    
+    if( !file_exists($dir1 . DIRECTORY_SEPARATOR . $filename) ) {
+        
+        $file = fopen($dir1, DIRECTORY_SEPARATOR . $filename, "w+");
+        
+        fwrite($file, date("Y-m_d H:i:s"));
+        
+        fclose($file);
+        
+    }
+    
+    rename(
+       $dir1 . DIRECTORY_SEPARATOR . $filename, // origem
+       $dir2 . DIRECTORY_SEPARATOR . $filename  // destino
+    );
+    
+    echo "Arquivo movido com sucesso!!!";
+
