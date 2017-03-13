bde95dbe74c1c1d0ef3bcfb63051815358b7c81f
jefflopo <jeffersonlopo@gmail.com>
2017-03-13 14:52:03 -0300

    Adicionada a pasta erros com exemplos de funcionamento

---
+++ erros/exemplo-01.php
@@ -0,0 +1,16 @@
+<?php
+    function error_handler( $code, $message, $file, $line ) {
+        
+        echo json_encode( array(
+            "code"=>$code,
+            "message"=>$message,
+            "line"=>$line,
+            "file"=>$file
+        ));
+        
+    }
+    
+    set_error_handler("error_handler");
+    
+    $total = 100 / 0;
+
