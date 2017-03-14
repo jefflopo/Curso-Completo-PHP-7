edfe8afdce9356f9332e7665e1d18eead7989f51
jefflopo <jeffersonlopo@gmail.com>
2017-03-14 08:31:33 -0300

    Implementado codigo try...catch no arquivo exemplo-01.php

--- try/exemplo-01.php
+++ try/exemplo-01.php
@@ -1,3 +1,16 @@
 <?php
-    
+    try{
+        
+        throw new Exception("Houve um erro", 400);
+        
+    } catch (Exception $exc) {
+        
+        echo json_encode(array(
+            "message"=>$exc->getMessage(),
+            "line"=>$exc->getLine(),
+            "file"=>$exc->getFile(),
+            "code"=>$exc->getCode()
+        ));
+        
+    }
 
