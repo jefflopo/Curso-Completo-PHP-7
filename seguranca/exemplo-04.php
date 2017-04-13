8416373c2b6add0bb72d15428b4ec65385ceeae0
jefflopo <jeffersonlopo@gmail.com>
2017-04-13 11:26:40 -0300

    Exemplo para envio de emails com o recaptcha do Google

---
+++ seguranca/exemplo-04.php
@@ -0,0 +1,10 @@
+<script src='https://www.google.com/recaptcha/api.js'></script>
+<form action="cadastrar.php" method="post">
+    
+    <div class="g-recaptcha" data-sitekey="6Ldw8xwUAAAAACFwa2SKu3dwfBasx_BsNU8HauL7"></div>
+    <input type="email" name="inputEmail">
+    <button type="submit">Enviar</button>
+        
+    
+</form>
+
