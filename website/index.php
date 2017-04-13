b03eaf0bf9bd19ee2d2366c4b84a059d27bcfd4c
jefflopo <jeffersonlopo@gmail.com>
2017-04-13 09:35:33 -0300

    Configuradas rotas para a pagina raiz / e para /hello/

--- website/index.php
+++ website/index.php
@@ -1,2 +1,15 @@
 <?php
-    phpinfo();
+    require_once ("vendor/autoload.php");
+    
+    $app = new \Slim\Slim();
+    
+    $app->get('/', function(){
+        echo json_encode(array(
+            'date'=>date("Y-m-d H:i:s")
+        ));
+    });
+    
+    $app->get('/hello/:name', function ($name) {
+        echo "Hello, " . $name;
+    });
+    $app->run();
