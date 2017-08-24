f5d83187b56094730b0e101600aecbdd5a539694
jefflopo <jeffersonlopo@gmail.com>
2017-08-24 10:03:51 -0300

    Retorno do Projeto Ecommerce

--- ecommerce/index.php
+++ ecommerce/index.php
@@ -9,7 +9,7 @@ $app->config('debug', true);
 $app->get('/', function() {
     
 	$sql = new Hcode\DB\Sql();
-        $results = $sql->select("SELECT * FROM tb_users");
+        $results = $sql->select("SELECT * FROM tb_usuarios");
         
         echo json_encode($results);
 
