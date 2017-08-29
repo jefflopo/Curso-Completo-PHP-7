69f0d40974b291ef459852dafd3ddf15d7f2b4a8
jefflopo <jeffersonlopo@gmail.com>
2017-08-29 10:23:53 -0300

    Configurado o template do header

--- ecommerce/vendor/hcodebr/php-classes/src/Page.php
+++ ecommerce/vendor/hcodebr/php-classes/src/Page.php
@@ -6,7 +6,16 @@ use Rain\Tpl;
 
 class Page {
     
-    public function __construct() {
+    private $tpl;
+    private $options = [];
+    private $defaults = [
+        "data"=>[]
+    ];
+
+
+    public function __construct($opts = array()) {
+        
+        $this->options = array_merge( $this->defaults, $opts );
         
         $config = array(
                     "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/view/",
@@ -15,6 +24,14 @@ class Page {
                 );
 
 	Tpl::configure( $config );
+        
+        $this->tpl = new Tpl;
+        
+        foreach ($this->options["data"] as $key =>$value){
+            $this->tpl->assign($key, $value);
+        }
+        
+        $this->tpl->draw("header");
         
     }
     
