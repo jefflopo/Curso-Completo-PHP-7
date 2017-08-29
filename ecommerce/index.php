635d195e76ea4a75da49e8ab1eaae2809c4545ad
jefflopo <jeffersonlopo@gmail.com>
2017-08-29 10:59:16 -0300

    Otimizado o codigo dos namespaces utilizados

--- ecommerce/index.php
+++ ecommerce/index.php
@@ -1,17 +1,19 @@
 <?php 
 
 require_once("vendor/autoload.php");
+use \Slim\Slim;
+use Hcode\DB\Sql;
 
-$app = new \Slim\Slim();
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-       $sql = new Hcode\DB\Sql();
-       $results = $sql->select("SELECT * FROM tb_users");
-        
-       echo json_encode($results);
+//       $sql = new Sql();
+//       $results = $sql->select("SELECT * FROM tb_users");
+//        
+//       echo json_encode($results);
 
 });
 
