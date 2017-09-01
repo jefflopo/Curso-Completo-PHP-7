12624c682ff9b6b184d5521bfce9dd5f43d70740
jefflopo <jeffersonlopo@gmail.com>
2017-09-01 10:01:52 -0300

    Criados arquivos de cache para cada parte do template

---
+++ ecommerce/views-cache/header.02dede634853cb2b2b097d8e2d4407c9.rtpl.php
@@ -0,0 +1,16 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Hcode Store</title>
+        
+    </head>
+    <body>
+        
+        
