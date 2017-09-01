95e9d5534dbe8b6a6090c87ec34152995be21a25
jefflopo <jeffersonlopo@gmail.com>
2017-09-01 12:00:11 -0300

    Criada a pagina PageAdmin

---
+++ ecommerce/vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,11 @@
+<?php
+namespace Hcode;
+
+class PageAdmin extends Page {
+    
+    public function __construct($opts = array(), $tpl_dir = "/CursoCompletoPHP7/ecommerce/views/admin/") {
+        parent::__construct($opts, $tpl_dir);
+    }
+    
+}
+
