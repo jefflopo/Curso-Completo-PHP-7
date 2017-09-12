d0451eaf5233cbccc0b03671f5ffedadc87a4e72
jefflopo <jeffersonlopo@gmail.com>
2017-09-12 11:12:43 -0300

    Criada a variavel constante de SESSION

--- ecommerce/vendor/hcodebr/php-classes/src/Model/User.php
+++ ecommerce/vendor/hcodebr/php-classes/src/Model/User.php
@@ -7,6 +7,8 @@ use \Hcode\Model;
 
 class User extends Model {
     
+    const SESSION = "User";
+    
     public static function login($login, $password){
         
         $sql = new Sql();
@@ -26,11 +28,32 @@ class User extends Model {
             
             $user->setData( $data );
             
-            var_dump($user);
-            exit;
+            $_SESSION[User::SESSION] = $user->getValues();
+                    
+            return $user;
         } else{
             throw new \Exception("Usuário inexistente ou senha inválida!!");
         }
+        
+    }
+    
+    public static function verifyLogin($inadmin = true){
+        
+        if( 
+                !isset($_SESSION[User::SESSION]) || 
+                !$_SESSION[User::SESSION] ||
+                !(int) $_SESSION[User::SESSION]["iduser"] > 0 ||
+                (bool) $_SESSION[User::SESSION]["inadmin"] !== $inadmin
+        ){
+            header("Location: ../admin/login");
+            exit;
+        }
+        
+    }
+    
+    public static function logout(){
+        
+        $_SESSION[User::SESSION] = NULL;
         
     }
     
