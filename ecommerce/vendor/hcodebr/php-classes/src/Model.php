d14c4fdec9014689b7f49670dcc7c6d45a786c5f
jefflopo <jeffersonlopo@gmail.com>
2017-09-12 09:09:41 -0300

    Criada a classe Model para gerar getters e setters

--- ecommerce/vendor/hcodebr/php-classes/src/Model.php
+++ ecommerce/vendor/hcodebr/php-classes/src/Model.php
@@ -7,7 +7,11 @@ class Model {
     
     public function __call($name, $args) {
         
+        $method = substr($name, 0, 3);
+        $fieldName = substr($name, 3, strlen($name));
         
+        var_dump($method, $fieldName);
+        exit;
         
     }
     
