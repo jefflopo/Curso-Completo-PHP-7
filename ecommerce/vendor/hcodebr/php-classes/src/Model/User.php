94d9b44c2782aff7a504551587df2088c6c5a544
jefflopo <jeffersonlopo@gmail.com>
2017-09-13 08:45:15 -0300

    Definida a sessao para o ambiente administrativo

--- ecommerce/vendor/hcodebr/php-classes/src/Model/User.php
+++ ecommerce/vendor/hcodebr/php-classes/src/Model/User.php
@@ -42,10 +42,10 @@ class User extends Model {
         if( 
                 !isset($_SESSION[User::SESSION]) || 
                 !$_SESSION[User::SESSION] ||
-                !(int) $_SESSION[User::SESSION]["iduser"] > 0 ||
-                (bool) $_SESSION[User::SESSION]["inadmin"] !== $inadmin
+                !(int)$_SESSION[User::SESSION]["iduser"] > 0 ||
+                (bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
         ){
-            header("Location: ../admin/login");
+            header("Location: ../ecommerce/admin/login");
             exit;
         }
         
