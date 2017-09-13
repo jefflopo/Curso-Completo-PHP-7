2059ba9c511ea4f2554f1cdcb82d30da8c19b442
jefflopo <jeffersonlopo@gmail.com>
2017-09-13 09:34:49 -0300

    Rotas pre-programadas para manipular os usuarios

--- ecommerce/index.php
+++ ecommerce/index.php
@@ -58,6 +58,54 @@ $app->get('/admin/logout', function(){
    exit;
 });
 
+$app->get("/admin/users", function(){
+    
+    User::verifyLogin();
+    
+    $page = new PageAdmin();
+    
+    $page->setTpl("users");
+    
+});
+
+$app->get("/admin/users/create", function(){
+    
+    User::verifyLogin();
+    
+    $page = new PageAdmin();
+    
+    $page->setTpl("users-create");
+    
+});
+
+$app->get("/admin/users/:iduser", function($iduser){
+    
+    User::verifyLogin();
+    
+    $page = new PageAdmin();
+    
+    $page->setTpl("users-update");
+    
+});
+
+$app->post("/admin/users/create", function(){
+    
+    User::verifyLogin();
+    
+});
+
+$app->post("/admin/users/:iduser", function($iduser){
+    
+    User::verifyLogin();
+    
+});
+
+$app->delete("/admin/users/:iduser", function($iduser){
+    
+    User::verifyLogin();
+    
+});
+
 $app->run();
 
  ?>
