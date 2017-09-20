a9501d52fb1d11dd4c1ad0ec6b675d47ea541e38
jefflopo <jeffersonlopo@gmail.com>
2017-09-20 09:48:52 -0300

    Adicionado o template de recuperar senha na tela de login

--- ecommerce/index.php
+++ ecommerce/index.php
@@ -149,6 +149,16 @@ $app->post("/admin/users/:iduser", function($iduser){
     
 });
 
+$app->get('/admin/forgot', function(){
+
+    $page = new PageAdmin( [
+        "header"=>false,
+        "footer"=>false
+    ]);
+    $page->setTpl("forgot");
+    
+});
+
 
 
 $app->run();
--- ecommerce/views-cache/forgot.eff21f71d259c66538aa03e75d3064ea.rtpl.php
+++ ecommerce/views-cache/forgot.eff21f71d259c66538aa03e75d3064ea.rtpl.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
