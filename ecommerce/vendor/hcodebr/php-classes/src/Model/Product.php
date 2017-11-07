ff181dfa62e51ef685f0615d5b2069908e12b691
jefflopo <jeffersonlopo@gmail.com>
2017-11-07 11:55:42 -0200

    Criada a classe Product com seus metodos

--- ecommerce/vendor/hcodebr/php-classes/src/Model/Product.php
+++ ecommerce/vendor/hcodebr/php-classes/src/Model/Product.php
@@ -6,13 +6,13 @@ use \Hcode\DB\Sql;
 use \Hcode\Model;
 use \Hcode\Mailer;
 
-class Category extends Model {
+class Product extends Model {
     
     public static function listAll(){
         
         $sql = new Sql();
         
-        return $sql->select("SELECT * FROM tb_categories ORDER BY descategory");
+        return $sql->select("SELECT * FROM tb_products ORDER BY desproduct");
         
     }
     
@@ -20,23 +20,27 @@ class Category extends Model {
         
         $sql = new Sql();
         
-        $results = $sql->select("CALL sp_categories_save(:idcategory, :descategory)",array(
-                            ":idcategory"=>$this->getidcategory(),
-                            ":descategory"=>$this->getdescategory()
+        $results = $sql->select("CALL sp_products_save(:idproduct, :desproduct, :vlprice, :vlwidth, :vlheight, :vllength, :vlweight, :desurl)",array(
+                            ":idproduct"=>$this->getidproduct(),
+                            ":desproduct"=>$this->getdesproduct(),
+                            ":vlprice"=>$this->getvlprice(),
+                            ":vlwidth"=>$this->getvlwidth(),
+                            ":vlheight"=>$this->getvlheight(),
+                            ":vllength"=>$this->getvllength(),
+                            ":vlweight"=>$this->getvlweight(),
+                            ":desurl"=>$this->getdesurl()
         ));
         
         $this->setData($results[0]);
         
-        Category::updateFile();
-        
     }
     
-    public function get($idcategory){
+    public function get($idproduct){
         
         $sql = new Sql();
         
-        $results = $sql->select("SELECT * FROM tb_categories WHERE idcategory = :idcategory",[
-            ":idcategory"=>$idcategory
+        $results = $sql->select("SELECT * FROM tb_products WHERE idproduct = :idproduct",[
+            ":idproduct"=>$idproduct
         ]);
         
         $this->setData($results[0]);
@@ -47,28 +51,11 @@ class Category extends Model {
         
         $sql = new Sql();
         
-        $sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory",[
-            ":idcategory"=>$this->getidcategory()
+        $sql->query("DELETE FROM tb_products WHERE idproduct = :idproduct",[
+            ":idproduct"=>$this->getidproduct()
         ]);
         
-        Category::updateFile();
         
-    }
-    
-    public static function updateFile(){
-        
-        $categories = Category::listAll();
-        $html = [];
-        
-        foreach ($categories as $row ) {
-            
-            array_push($html, '<li><a href="/CursoCompletoPHP7/ecommerce/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
-            
-        }
-        
-        file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR ."CursoCompletoPHP7" . DIRECTORY_SEPARATOR . "ecommerce" . DIRECTORY_SEPARATOR . "views" .DIRECTORY_SEPARATOR. "categories-menu.html", implode('', $html));
-        /* $_SERVER['DOCUMENT_ROOT']*/
-//        echo "DOCUMENT_ROOT" . $_SERVER['DOCUMENT_ROOT'] ;
     }
 
     
