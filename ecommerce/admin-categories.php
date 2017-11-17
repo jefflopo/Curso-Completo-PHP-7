b2129128f37d4a2a9af5e14d0912c2b066eaa34c
jefflopo <jeffersonlopo@gmail.com>
2017-11-17 09:04:22 -0200

    Adicionadas template para categorias de produtos e rotas de add e remove

--- ecommerce/admin-categories.php
+++ ecommerce/admin-categories.php
@@ -3,6 +3,7 @@
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
 use \Hcode\Model\Category;
+use \Hcode\Model\Product;
 use \Hcode\PageCategories;
 
 $app->get("/admin/categories", function(){
@@ -107,17 +108,55 @@ $app->post("/admin/categories/:idcategory", function($idcategory){
     
 });
 
-$app->get("/categories/:idcategory", function($idcategory){
+$app->get("/admin/categories/:idcategory/products", function($idcategory){
+    
+    User::verifyLogin();
     
     $category = new Category();
     
     $category->get((int)$idcategory);
     
-    $page = new PageCategories();
+    $page = new PageAdmin();
     
-    $page->setTpl("category", [
-        'category'=>$category->getValues()
+    $page->setTpl("categories-products", [
+        'category'=>$category->getValues(),
+        'productsRelated'=>$category->getProducts(),
+        'productsNotRelated'=>$category->getProducts(false)
     ]);
+});
+
+$app->get("/admin/categories/:idcategory/products/:idproduct/add", function($idcategory, $idproduct){
     
+    User::verifyLogin();
+    
+    $category = new Category();
+    
+    $category->get((int)$idcategory);
+    
+    $product = new Product();
+    
+    $product->get((int)$idproduct);
+    
+    $category->addProduct($product);
+    
+    header("Location: /CursoCompletoPHP7/ecommerce/admin/categories/" .$idcategory. "/products");
+    exit;
+});
+$app->get("/admin/categories/:idcategory/products/:idproduct/remove", function($idcategory, $idproduct){
+    
+    User::verifyLogin();
+    
+    $category = new Category();
+    
+    $category->get((int)$idcategory);
+    
+    $product = new Product();
+    
+    $product->get((int)$idproduct);
+    
+    $category->removeProduct($product);
+    
+    header("Location: /CursoCompletoPHP7/ecommerce/admin/categories/" .$idcategory. "/products");
+    exit;
 });
 
