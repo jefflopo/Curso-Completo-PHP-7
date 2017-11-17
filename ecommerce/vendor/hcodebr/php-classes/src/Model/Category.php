e408a4bcfad0c7808adfc6f8a1a50a15f1865009
jefflopo <jeffersonlopo@gmail.com>
2017-11-17 11:54:27 -0200

    Definido o metodo getProductsPage

--- ecommerce/vendor/hcodebr/php-classes/src/Model/Category.php
+++ ecommerce/vendor/hcodebr/php-classes/src/Model/Category.php
@@ -105,6 +105,32 @@ class Category extends Model {
         
     }
     
+    public function getProductsPage($page = 1, $itemsPerPage = 3){
+        
+        $start = ($page - 1) * $itemsPerPage;
+        
+        $sql = new Sql();
+        
+        $results = $sql->select("
+                    SELECT SQL_CALC_FOUND_ROWS *
+                    FROM tb_products a
+                    INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
+                    INNER JOIN tb_categories c ON c.idcategory = b.idcategory
+                    WHERE c.idcategory = :idcategory
+                    LIMIT $start, $itemsPerPage;
+        ", [
+            ':idcategory'=>$this->getidcategory()
+        ]);
+        
+        $resultsTotal = $sql->select("SELECT FOUND_ROWS() AS regtotal;");
+        
+        return [
+            'data'=>Product::checkList($results),
+            'total'=>(int)$resultsTotal[0]["regtotal"],
+            'pages'=>ceil( $resultsTotal[0]["regtotal"] / $itemsPerPage )
+        ];
+    }
+    
     public function addProduct(Product $product) {
         
         $sql = new Sql();
