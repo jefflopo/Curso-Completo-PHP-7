d9ae68e2a96ab2e28abc943fcb5b8a58452a20a8
jefflopo <jeffersonlopo@gmail.com>
2017-11-20 12:00:14 -0200

    Atualizada a classe para checar se ha usuario logado

--- ecommerce/vendor/hcodebr/php-classes/src/Model/User.php
+++ ecommerce/vendor/hcodebr/php-classes/src/Model/User.php
@@ -11,6 +11,46 @@ class User extends Model {
     const SESSION = "User";
     const SECRET = "HcodePhp7_Secret";
     
+    public static function getFromSession(){
+        
+        $user = new User();
+        
+        if(isset($_SESSION[User::SESSION]) && (int)$_SESSION[User::SESSION]['iduser'] > 0){
+            
+            $user->setData($_SESSION[User::SESSION]);
+            
+        }
+        
+        return $user;
+        
+    }
+    
+    public static function checkLogin($inadmin = true){
+        
+        if(
+                !isset($_SESSION[User::SESSION]) 
+                || 
+                !$_SESSION[User::SESSION] 
+                ||
+                !(int)$_SESSION[User::SESSION]["iduser"] > 0
+            ){
+            //USUARIO LOGADO!
+            return false;
+            
+        }else{
+            
+            if($inadmin === true && $_SESSION[User::SESSION]['inadmin'] === true){
+                return true;
+            }else if($inadmin === false){
+                return true;
+            } else{
+                return false;
+            }
+            
+        }
+        
+    }
+    
     public static function login($login, $password){
         
         $sql = new Sql();
@@ -44,15 +84,12 @@ class User extends Model {
 //        var_dump($_SESSION[User::SESSION]);
 //        exit; 
         
-        if( 
-                !isset($_SESSION[User::SESSION]) || 
-                !$_SESSION[User::SESSION] ||
-                !(int)$_SESSION[User::SESSION]["iduser"] > 0 ||
-                (bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
-        ){
+        if(User::checkLogin($inadmin)){
+            
             header("Location: ../ecommerce/admin/login");
             exit;
             echo "Erro ao logar no sistema!";
+            
         }else{
 //            echo "Sucesso ao logar no sistema!";
             
