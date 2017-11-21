66e8c5e7221aac7a3bc6bcff336f96d2445e5711
jefflopo <jeffersonlopo@gmail.com>
2017-11-21 12:11:42 -0200

    Desenvolvida toda a parte de inserir no carrinho e verificar produtos inseridos

--- ecommerce/views-cache/index.02dede634853cb2b2b097d8e2d4407c9.rtpl.php
+++ ecommerce/views-cache/index.02dede634853cb2b2b097d8e2d4407c9.rtpl.php
@@ -90,12 +90,12 @@
                                     <div class="product-f-image">
                                         <img src="<?php echo htmlspecialchars( $value1["desphoto"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" alt="">
                                         <div class="product-hover">
-                                            <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Comprar</a>
-                                            <a href="/products/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" class="view-details-link"><i class="fa fa-link"></i> Ver Detalhes</a>
+                                            <a href="../ecommerce/cart/<?php echo htmlspecialchars( $value1["idproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?>/add" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Comprar</a>
+                                            <a href="../ecommerce/products/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" class="view-details-link"><i class="fa fa-link"></i> Ver Detalhes</a>
                                         </div>
                                     </div>
 
-                                    <h2><a href="/products/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["desproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?></a></h2>
+                                    <h2><a href="../ecommerce/products/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["desproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?></a></h2>
 
                                     <div class="product-carousel-price">
                                         <ins>R$<?php echo formatPrice($value1["vlprice"]); ?></ins> 
