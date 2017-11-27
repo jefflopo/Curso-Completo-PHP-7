83f642af310d9843ebfe4b480561f7f03c28fb9d
jefflopo <jeffersonlopo@gmail.com>
2017-11-27 10:20:29 -0200

    Alteracoes no Minha Conta

--- ecommerce/site.php
+++ ecommerce/site.php
@@ -292,3 +292,66 @@ $app->post("/forgot/reset", function(){
     
     
 });
+$app->get("/profile", function(){
+    
+    User::verifyLogin(false);
+    
+    $user = User::getFromSession();
+    
+    $page = new Page();
+    
+    $page->setTpl("profile",[
+        'user'=>$user->getValues(),
+        'profileMsg'=>User::getSuccess(),
+        'profileError'=>User::getError()
+    ]);
+    
+});
+$app->post("/profile", function(){
+    
+    User::verifyLogin(false);
+    
+    if(!isset($_POST['desperson']) || $_POST['desperson'] === ''){
+        
+        User::setError("Preencha o seu nome.");
+        header("Location: ../ecommerce/profile");
+        exit;
+        
+    }
+    if(!isset($_POST['desemail']) || $_POST['desemail'] === ''){
+        
+        User::setError("Preencha o seu e-mail.");
+        header("Location: ../ecommerce/profile");
+        exit;
+        
+    }
+    
+    $user = User::getFromSession();
+    
+    if($_POST['desemail'] !== $user->getdesemail()){
+        
+        if(User::checkLoginExists($_POST['desemail']) == true){
+            
+            User::setError("Este endereço de e-mail já está cadastrado.");
+            header("Location: ../ecommerce/profile");
+            exit;
+            
+        }
+        
+    }
+    
+    $user = User::getFromSession();
+    
+    $_POST['inadmin'] = $user->getinadmin();
+    $_POST['despassword'] = $user->getdespassword();
+    $_POST['deslogin'] = $_POST['desemail'];
+    
+    $user->setData($_POST);
+    
+    $user->save();
+    
+    User::setSuccess("Dados alterados com sucesso!");
+    
+    header("Location: ../ecommerce/profile");
+    exit;
+});
--- ecommerce/vendor/hcodebr/php-classes/src/Model/User.php
+++ ecommerce/vendor/hcodebr/php-classes/src/Model/User.php
@@ -12,6 +12,7 @@ class User extends Model {
     const SECRET = "HcodePhp7_Secret";
     const ERROR = "UserError";
     const ERROR_REGISTER = "UserErrorRegister";
+    const SUCCESS = "UserSuccess";
     
     public static function getFromSession(){
         
@@ -312,6 +313,27 @@ class User extends Model {
         $_SESSION[User::ERROR] = NULL;
         
     }
+    public static function setSuccess($msg){
+        
+        $_SESSION[User::SUCCESS] = $msg;
+        
+    }
+    
+    public static function getSuccess(){
+        
+        $msg = (isset($_SESSION[User::SUCCESS]) && $_SESSION[User::SUCCESS]) ? $_SESSION[User::SUCCESS] : '';
+        
+        User::clearSuccess();
+        
+        return $msg;
+        
+    }
+    
+    public static function clearSuccess(){
+        
+        $_SESSION[User::SUCCESS] = NULL;
+        
+    }
     
     public static function setErrorRegister($msg){
         
@@ -334,7 +356,7 @@ class User extends Model {
         
     }
     
-    public static function checkLoginExist($login){
+    public static function checkLoginExists($login){
         
         $sql = new Sql();
         
