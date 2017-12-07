e0f996e3967e73ff1a2e48cb7902f9d4e0ebe1a6
jefflopo <jeffersonlopo@gmail.com>
2017-12-07 11:03:09 -0200

    Admin - Pedidos - Busca e Paginacao

--- ecommerce/admin-products.php
+++ ecommerce/admin-products.php
@@ -35,8 +35,6 @@ $app->get("/admin/products", function(){
         
     }
     
-    $products = Product::listAll();
-    
     $page = new PageAdmin();
     
     $page->setTpl("products", [
--- ecommerce/vendor/hcodebr/php-classes/src/Model/Order.php
+++ ecommerce/vendor/hcodebr/php-classes/src/Model/Order.php
@@ -131,5 +131,63 @@ class Order extends Model {
         
     }
     
+    public static function getPage($page = 1, $itemsPerPage = 10){
+        
+        $start = ($page - 1) * $itemsPerPage;
+        
+        $sql = new Sql();
+        
+        $results = $sql->select("
+                    SELECT SQL_CALC_FOUND_ROWS *
+                    FROM tb_orders a 
+                    INNER JOIN tb_ordersstatus b USING(idstatus)
+                    INNER JOIN tb_carts c USING(idcart)
+                    INNER JOIN tb_users d ON d.iduser = a.iduser
+                    INNER JOIN tb_addresses e USING(idaddress)
+                    INNER JOIN tb_persons f ON f.idperson = d.idperson
+                    ORDER BY a.dtregister DESC
+                    LIMIT $start, $itemsPerPage;
+        ");
+        
+        $resultsTotal = $sql->select("SELECT FOUND_ROWS() AS regtotal;");
+        
+        return [
+            'data'=>$results,
+            'total'=>(int)$resultsTotal[0]["regtotal"],
+            'pages'=>ceil( $resultsTotal[0]["regtotal"] / $itemsPerPage )
+        ];
+    }
+    
+    public static function getPageSearch($search, $page = 1, $itemsPerPage = 10){
+        
+        $start = ($page - 1) * $itemsPerPage;
+        
+        $sql = new Sql();
+        
+        $results = $sql->select("
+                    SELECT SQL_CALC_FOUND_ROWS *
+                    FROM tb_orders a 
+                    INNER JOIN tb_ordersstatus b USING(idstatus)
+                    INNER JOIN tb_carts c USING(idcart)
+                    INNER JOIN tb_users d ON d.iduser = a.iduser
+                    INNER JOIN tb_addresses e USING(idaddress)
+                    INNER JOIN tb_persons f ON f.idperson = d.idperson
+                    WHERE a.idorder = :id OR f.desperson LIKE :search
+                    ORDER BY a.dtregister DESC
+                    LIMIT $start, $itemsPerPage;
+        ",[
+            ':search'=>'%'.$search.'%',
+            ':id'=>$search
+        ]);
+        
+        $resultsTotal = $sql->select("SELECT FOUND_ROWS() AS regtotal;");
+        
+        return [
+            'data'=>$results,
+            'total'=>(int)$resultsTotal[0]["regtotal"],
+            'pages'=>ceil( $resultsTotal[0]["regtotal"] / $itemsPerPage )
+        ];
+    }
+    
 }
 
