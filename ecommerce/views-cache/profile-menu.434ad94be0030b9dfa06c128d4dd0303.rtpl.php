bc8d657c3adbb5a31875440d19f073f635014a09
jefflopo <jeffersonlopo@gmail.com>
2017-12-07 11:49:17 -0200

    Admin - Usuarios Ajustes

--- ecommerce/views-cache/profile-menu.434ad94be0030b9dfa06c128d4dd0303.rtpl.php
+++
@@ -1,6 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><div class="list-group" id="menu">
-    <a href="../ecommerce/profile" class="list-group-item list-group-item-action">Editar Dados</a>
-    <a href="#" class="list-group-item list-group-item-action">Alterar Senha</a>
-    <a href="../ecommerce/profile/orders" class="list-group-item list-group-item-action">Meus Pedidos</a>
-    <a href="../ecommerce/logout" class="list-group-item list-group-item-action">Sair</a>
-</div>
