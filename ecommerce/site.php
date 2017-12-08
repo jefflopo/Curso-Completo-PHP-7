c40e0a28111d8bb83bdf2cb2d8af0a5759e3cc15
jefflopo <jeffersonlopo@gmail.com>
2017-12-08 11:39:55 -0200

    Integracao PayPal HTML

--- ecommerce/site.php
+++ ecommerce/site.php
@@ -278,7 +278,15 @@ $app->post("/checkout", function(){
     
     $order->save();
     
-    header("Location: ../ecommerce/order/".$order->getidorder()."/pagseguro");
+    switch ((int)$_POST['payment-method']){
+        case 1:
+            header("Location: ../ecommerce/order/".$order->getidorder()."/pagseguro");
+            break;
+        
+        case 2:
+            header("Location: ../ecommerce/order/".$order->getidorder()."/paypal");
+            break;
+    }
     exit;
     
 });
@@ -305,6 +313,28 @@ $app->get("/order/:idorder/pagseguro", function($idorder){
             'areaCode'=> substr($order->getnrphone(), 0,2),
             'number'=> substr($order->getnrphone(), 2, strlen($order->getnrphone()))
         ]
+    ]);
+    
+});
+$app->get("/order/:idorder/paypal", function($idorder){
+    
+    User::verifyLogin(false);
+    
+    $order = new Order();
+    
+    $order->get((int)$idorder);
+    
+    $cart = $order->getCart();
+    
+    $page = new Page([
+        'header'=>false,
+        'footer'=>false
+    ]);
+    
+    $page->setTpl("payment-paypal",[
+        'order'=>$order->getValues(),
+        'cart'=>$cart->getValues(),
+        'products'=>$cart->getProducts()
     ]);
     
 });
